8ebf946da2b574c16245bddec57c06609ad1b72d
Marko19907 <marko.stancic23@gmail.com>
2021-05-03 10:56:43 +0200

    Added more unit tests

--- src/test/java/no/ntnu/mappe2/marko19907/patientregister/PatientRegisterPlainTest.java
+++ src/test/java/no/ntnu/mappe2/marko19907/patientregister/PatientRegisterPlainTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class PatientRegisterPlainTest
 {
@@ -94,5 +95,63 @@ public class PatientRegisterPlainTest
 
         assertEquals(1, register.getNumberOfPatients());
         assertFalse(removed);
+    }
+
+    @Test
+    @DisplayName("Test adding a duplicate Patient to the register")
+    public void testAddingDuplicatePatient()
+    {
+        // Arrange
+        PatientRegisterPlain register = new PatientRegisterPlain();
+        Patient patient1 = new Patient("Name1", "LastName1", "123");
+        Patient patient2 = new Patient("Name2", "LastName2", "123");
+        boolean exceptionThrown = false;
+
+
+        // Act
+        register.addPatient(patient1);
+        try {
+            register.addPatient(patient2);
+            fail("An exception should have been thrown!");
+        }
+        catch (DuplicateKeyException e) {
+            exceptionThrown = true;
+        }
+
+
+        // Assert
+        assertEquals(1, register.getNumberOfPatients());
+        assertTrue(exceptionThrown);
+        assertTrue(register.getPatientList().stream().anyMatch(patient -> patient.equals(patient1)));
+    }
+
+    @Test
+    @DisplayName("Test adding a duplicate Patient to the register from a List")
+    public void testAddingDuplicatePatientsFromList()
+    {
+        // Arrange
+        PatientRegisterPlain register = new PatientRegisterPlain();
+        Patient patient1 = new Patient("Name1", "LastName1", "123");
+        Patient patient2 = new Patient("Name2", "LastName2", "123");
+        boolean exceptionThrown = false;
+        List<Patient> patientList = new ArrayList<>();
+        patientList.add(patient1);
+        patientList.add(patient2);
+
+
+        // Act
+        try {
+            register.addPatients(patientList);
+            fail("An exception should have been thrown!");
+        }
+        catch (DuplicateKeyException e) {
+            exceptionThrown = true;
+        }
+
+
+        // Assert
+        assertEquals(1, register.getNumberOfPatients());
+        assertTrue(exceptionThrown);
+        assertTrue(register.getPatientList().stream().anyMatch(patient -> patient.equals(patient1)));
     }
 }
